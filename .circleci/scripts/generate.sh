745f81876b737f2adccbfaf2542a62f40961d1b1
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2024-09-27 20:02:50 +0200

    .15

--- .circleci/scripts/generate.sh
+++ .circleci/scripts/generate.sh
@@ -1,6 +1,6 @@
 #@IgnoreInspection BashAddShebang
 SERVICE="charon"
-PROTO_INCLUDE="-I=./tmp/pb -I=./vendor/github.com/piotrkowalczuk -I=${GOPATH}/src"
+PROTO_INCLUDE="-I=./tmp/pb -I=/home/circleci/project/vendor/github.com/piotrkowalczuk -I=/home/circleci/project/pb"
 
 : ${PROTOC:="${PWD}/tmp/bin/protoc"}
 
